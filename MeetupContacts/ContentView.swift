627cdbd1befda5e24a0c513f4f3e37462daee181
Esben Viskum <viskum@viskum.dk>
2021-05-17 12:19:58 +0200

    Initial Commit

---
+++ MeetupContacts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MeetupContacts
+//
+//  Created by Esben Viskum on 17/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MeetupContacts/MeetupContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetupContactsApp.swift
+//  MeetupContacts
+//
+//  Created by Esben Viskum on 17/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MeetupContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
